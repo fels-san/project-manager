0bbe49f33a4b1baf99ddfd495870bab2340c0043
= <=>
2025-01-21 16:10:19 +0300

    Fix eslint errors in sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
-import { useContext } from "react";
+import { React, useContext } from 'react';
 
-import { ProjectManagementContext } from "../store/project-management-context";
+import { ProjectManagementContext } from '../store/project-management-context';
 
 export default function Sidebar() {
   const { projects, createProject, selectProject } = useContext(
@@ -11,7 +11,7 @@ export default function Sidebar() {
     (project) => project.isCompleted === false
   );
   const completedProjects = projects.filter(
-    (project) => project.isCompleted == true
+    (project) => project.isCompleted === true
   );
 
   return (
@@ -20,6 +20,7 @@ export default function Sidebar() {
         Your projects
       </h2>
       <button
+        type="button"
         onClick={createProject}
         className="bg-stone-700 text-stone-400 rounded-md px-4 py-2 my-7"
       >
@@ -28,39 +29,37 @@ export default function Sidebar() {
       <h3 className="text-stone-300 font-bold text-lg mb-4">
         Current Projects
       </h3>
-      {currentProjects.map((project) => {
-        return (
-          <button
-            onClick={() => selectProject(project.id)}
-            key={project.id}
-            className={`${
-              project.isSelected
-                ? "bg-stone-800 text-stone-300"
-                : "text-stone-400"
-            } text-left w-full mb-3 p-2`}
-          >
-            {project.title}
-          </button>
-        );
-      })}
+      {currentProjects.map((project) => (
+        <button
+          type="button"
+          onClick={() => selectProject(project.id)}
+          key={project.id}
+          className={`${
+            project.isSelected
+              ? 'bg-stone-800 text-stone-300'
+              : 'text-stone-400'
+          } text-left w-full mb-3 p-2`}
+        >
+          {project.title}
+        </button>
+      ))}
       <h3 className="text-stone-400 font-bold text-lg mb-4">
         Completed Projects
       </h3>
-      {completedProjects.map((project) => {
-        return (
-          <button
-            onClick={() => selectProject(project.id)}
-            key={project.id}
-            className={`${
-              project.isSelected
-                ? "bg-stone-800 text-stone-300"
-                : "text-stone-400"
-            } text-left w-full mb-3 p-2`}
-          >
-            {project.title}
-          </button>
-        );
-      })}
+      {completedProjects.map((project) => (
+        <button
+          type="button"
+          onClick={() => selectProject(project.id)}
+          key={project.id}
+          className={`${
+            project.isSelected
+              ? 'bg-stone-800 text-stone-300'
+              : 'text-stone-400'
+          } text-left w-full mb-3 p-2`}
+        >
+          {project.title}
+        </button>
+      ))}
     </aside>
   );
 }
