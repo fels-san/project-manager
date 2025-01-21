bfa6994e233bfbe21b257acb6ea01f1e86c9b48e
= <=>
2025-01-21 15:57:49 +0300

    Fix eslint errors in edit employee

--- src/components/currentPage/EditEmployee.jsx
+++ src/components/currentPage/EditEmployee.jsx
@@ -1,8 +1,8 @@
-import { useState, useContext } from "react";
+import { React, useState, useContext } from 'react';
 
-import { ProjectManagementContext } from "../../store/project-management-context";
+import { ProjectManagementContext } from '../../store/project-management-context';
 
-import InputField from "./projectForm/InputField";
+import InputField from './projectForm/InputField';
 
 export default function EditEmployee({ employee }) {
   const { updateEmployee } = useContext(ProjectManagementContext);
@@ -19,7 +19,7 @@ export default function EditEmployee({ employee }) {
 
   const handleChange = (field, value) => {
     const updatedValue =
-      (field === "birthDate" || field === "companyStartYear") && value
+      (field === 'birthDate' || field === 'companyStartYear') && value
         ? new Date(value)
         : value;
 
@@ -28,7 +28,7 @@ export default function EditEmployee({ employee }) {
 
   const handleSave = () => {
     if (!formData.name) {
-      alert("Name are required");
+      alert('Name are required');
       return;
     }
     updateEmployee(formData);
@@ -39,40 +39,41 @@ export default function EditEmployee({ employee }) {
       <InputField
         label="Name"
         defaultValue={formData.name}
-        onChange={(e) => handleChange("name", e.target.value)}
+        onChange={(e) => handleChange('name', e.target.value)}
       />
       <InputField
         label="Role"
         defaultValue={formData.position}
-        onChange={(e) => handleChange("position", e.target.value)}
+        onChange={(e) => handleChange('position', e.target.value)}
       />
       <InputField
         label="Birthdate"
         type="date"
         defaultValue={formData.birthDate}
-        onChange={(e) => handleChange("birthDate", e.target.value)}
+        onChange={(e) => handleChange('birthDate', e.target.value)}
       />
       <InputField
         label="Joined the company"
         type="date"
         defaultValue={formData.companyStartYear}
-        onChange={(e) => handleChange("companyStartYear", e.target.value)}
+        onChange={(e) => handleChange('companyStartYear', e.target.value)}
       />
       <InputField
         label="Phone"
         type="tel"
         defaultValue={formData.phone}
-        onChange={(e) => handleChange("phone", e.target.value)}
+        onChange={(e) => handleChange('phone', e.target.value)}
       />
       <InputField
         label="Email"
         type="email"
         defaultValue={formData.email}
-        onChange={(e) => handleChange("email", e.target.value)}
+        onChange={(e) => handleChange('email', e.target.value)}
       />
       <div className="flex flex-row justify-end gap-3 mt-4">
-        <button>Cancel</button>
+        <button type="button">Cancel</button>
         <button
+          type="button"
           onClick={handleSave}
           className="bg-stone-700 text-stone-50 rounded-md px-6 py-2"
         >
