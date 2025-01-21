31ad5b9df46fff0dade1b026afbce28b99d1ce95
= <=>
2025-01-21 15:49:42 +0300

    Fix key error in List Display

--- src/components/currentPage/projectForm/ListDisplay.jsx
+++ src/components/currentPage/projectForm/ListDisplay.jsx
@@ -10,16 +10,17 @@ export default function ListDisplay({ list, setList }) {
   return (
     <div className="flex flex-row gap-3">
       {list.map((item) => (
-        <button
-          type="button"
-          key={item.name}
-          onClick={() => handleDeleteItem(item)}
-          className="flex flex-row justify-between gap-2 text-stone-700 text-base font-semibold h-min border-stone-900 border-2 rounded-full px-4 py-0 mt-2 text-center items-center cursor-pointer transform transition-transform duration-75 hover:scale-105"
-        >
-          {item.name ? item.name : item}
-          <img className="w-5 h-min" src={Delete} alt="delete" />
-        </button>
-      ))}
+          <button
+            type="button"
+            key={item.id ? item.id : item}
+            onClick={() => handleDeleteItem(item)}
+            className="flex flex-row justify-between gap-2 text-stone-700 text-base font-semibold h-min border-stone-900 border-2 rounded-full px-4 py-0 mt-2 text-center items-center cursor-pointer transform transition-transform duration-75 hover:scale-105"
+          >
+            {item.name ? item.name : item}
+            <img className="w-5 h-min" src={Delete} alt="delete" />
+          </button>
+        )
+      )}
     </div>
   );
 }
