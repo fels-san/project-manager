8a9966931b3a2ada795e2d2922bc8dad4a8a9876
Anastasia Krasnova <a.dkrasnova@yandex.ru>
2025-02-24 16:03:56 +0300

    feat: add basename to router for correct routing

--- src/App.tsx
+++ src/App.tsx
@@ -8,21 +8,26 @@ import EmployeeProfile from "./pages/EmployeeProfile";
 import EditEmployee from "./pages/EditEmployee";
 import Statitic from "./pages/Statitic";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <RootLayout />,
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "/project/new", element: <ProjectForm /> },
+        { path: "/project/:projectId", element: <Project /> },
+        { path: "/project/:projectId/edit", element: <ProjectForm /> },
+        { path: "/employee/:employeeName", element: <EmployeeProfile /> },
+        { path: "/employee/:employeeName/edit", element: <EditEmployee /> },
+        { path: "/statitic", element: <Statitic /> },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <RootLayout />,
-    children: [
-      { path: "/", element: <Home /> },
-      { path: "/project/new", element: <ProjectForm /> },
-      { path: "/project/:projectId", element: <Project /> },
-      { path: "/project/:projectId/edit", element: <ProjectForm /> },
-      { path: "/employee/:employeeName", element: <EmployeeProfile /> },
-      { path: "/employee/:employeeName/edit", element: <EditEmployee /> },
-      { path: "/statitic", element: <Statitic /> },
-    ],
-  },
-]);
+    basename: "/project-manager/",
+  }
+);
 
 function App() {
   return <RouterProvider router={router} />;
