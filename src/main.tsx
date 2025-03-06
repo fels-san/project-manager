63a3a7745e3523916c0c32202e5bdc07c201339d
Anastasia Krasnova <a.dkrasnova@yandex.ru>
2025-03-06 15:03:58 +0300

    feat: add redux-persist to save projects in localStorage

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-import store from './store/index';
-import App from './App';
-import './index.css';
+import store, { persistor } from "./store/index";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
